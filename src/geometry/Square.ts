1a14d925b494d561456939f2aebc1b210a7b642a
Yuhan Liu <02yuhanliu@gmail.com>
2023-09-14 23:31:21 -0400

    set up project

--- src/geometry/Square.ts
+++ src/geometry/Square.ts
@@ -2,9 +2,10 @@ import {vec3, vec4} from 'gl-matrix';
 import Drawable from '../rendering/gl/Drawable';
 import {gl} from '../globals';
 
-class Square extends Drawable {
+class Cube extends Drawable {
   indices: Uint32Array;
   positions: Float32Array;
+  normals: Float32Array;
   center: vec4;
 
   constructor(center: vec3) {
@@ -16,17 +17,25 @@ class Square extends Drawable {
 
   this.indices = new Uint32Array([0, 1, 2,
                                   0, 2, 3]);
-  this.positions = new Float32Array([-1, -1, 0.999, 1,
-                                     1, -1, 0.999, 1,
-                                     1, 1, 0.999, 1,
-                                     -1, 1, 0.999, 1]);
+  this.normals = new Float32Array([0, 0, 1, 0,
+                                   0, 0, 1, 0,
+                                   0, 0, 1, 0,
+                                   0, 0, 1, 0]);
+  this.positions = new Float32Array([-1, -1, 0, 1,
+                                     1, -1, 0, 1,
+                                     1, 1, 0, 1,
+                                     -1, 1, 0, 1]);
 
     this.generateIdx();
     this.generatePos();
+    this.generateNor();
 
     this.count = this.indices.length;
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufIdx);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.bufNor);
+    gl.bufferData(gl.ARRAY_BUFFER, this.normals, gl.STATIC_DRAW);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this.bufPos);
     gl.bufferData(gl.ARRAY_BUFFER, this.positions, gl.STATIC_DRAW);
@@ -35,4 +44,4 @@ class Square extends Drawable {
   }
 };
 
-export default Square;
+export default Cube;
--- src/rendering/gl/OpenGLRenderer.ts
+++ src/rendering/gl/OpenGLRenderer.ts
@@ -6,11 +6,18 @@ import ShaderProgram from './ShaderProgram';
 
 // In this file, `gl` is accessible because it is imported above
 class OpenGLRenderer {
+
+  color: vec4;
+
   constructor(public canvas: HTMLCanvasElement) {
   }
 
   setClearColor(r: number, g: number, b: number, a: number) {
     gl.clearColor(r, g, b, a);
+  }
+
+  setCubeColor(color: number[]) {
+    this.color = vec4.fromValues(color[0]/255, color[1]/255, color[2]/255, 1);
   }
 
   setSize(width: number, height: number) {
@@ -23,7 +30,15 @@ class OpenGLRenderer {
   }
 
   render(camera: Camera, prog: ShaderProgram, drawables: Array<Drawable>, time: number) {
-    prog.setEyeRefUp(camera.controls.eye, camera.controls.center, camera.controls.up);
+    let model = mat4.create();
+    let viewProj = mat4.create();
+    let color = this.color;
+
+    mat4.identity(model);
+    mat4.multiply(viewProj, camera.projectionMatrix, camera.viewMatrix);
+    prog.setModelMatrix(model);
+    prog.setViewProjMatrix(viewProj);
+    prog.setGeometryColor(color);
     prog.setTime(time);
 
     for (let drawable of drawables) {
